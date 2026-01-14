bf7923781a565a1d12fd314802dc5faf9fdedd96
Isaac Vidrine <idvidrine@gmail.com>
2026-01-14 17:32:08 -0500

    add 404 and error pages

---
+++ app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="w-full mx-auto max-w-3xl py-12 px-12">
+      <h1 className="text-center ">Error</h1>
+      <p className="text-center text-sm text-muted-foreground">
+        Something went wrong
+      </p>
+    </main>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <main className="w-full mx-auto max-w-3xl py-12 px-12">
+      <h1 className="text-center ">404</h1>
+      <p className="text-center text-sm text-muted-foreground">Not found</p>
+    </main>
+  );
+}
