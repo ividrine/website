cd64c9acfd41010ccd717154b51ef6e2c4fb19fe
Isaac Vidrine <idvidrine@gmail.com>
2026-01-14 19:30:20 -0500

    remove blog implementation
    I realized dynamic pages meant monthly costs, so will do static blog pages in future

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,24 +1,9 @@
-import { getPosts } from "@/lib/posts";
-import Link from "next/link";
 export default function Blog() {
-  const posts = getPosts();
-
-  const postsContent = posts?.map((post, index) => (
-    <li key={index}>
-      <Link href={`/blog/${post.id}`}>{post.title}</Link>
-    </li>
-  ));
-
   return (
     <div className="flex items-center justify-center">
       <main className="w-full max-w-3xl py-12 px-12">
         <h1 className="text-center">Blog</h1>
-        {postsContent?.length === 0 && (
-          <p className="text-center">No blog posts yet :/</p>
-        )}
-        {postsContent?.length > 0 && (
-          <ul className="text-center list-disc list-inside">{postsContent}</ul>
-        )}
+        <p className="text-center">No blog posts yet :/</p>
       </main>
     </div>
   );
