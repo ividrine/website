35c5ded61f292383b6bdf8220eb345f1a8545fd3
Isaac Vidrine <idvidrine@gmail.com>
2026-01-14 19:40:48 -0500

    update webring copy

--- app/webring/page.tsx
+++ app/webring/page.tsx
@@ -5,19 +5,15 @@ export default function WebRing() {
     <main className="w-full mx-auto max-w-3xl py-12 px-12">
       <h1 className="text-center">Webring</h1>
       <p className="indent-6">
-        Together with some friends and colleagues, we are bringing back old
-        school{" "}
-        <Link target="_blank" href="https://en.wikipedia.org/wiki/Webring">
-          Webrings
-        </Link>
-        . You can check out their cool sites below:
+        I have added this site to a Webring that my friends and colleagues
+        started. You can check out their websites here:
       </p>
       <ul className="list-disc list-inside">
         <li>
-          <Link href="https://lanekatris.com">https://lanekatris.com</Link>
+          <Link href="https://lanekatris.com/webring">Lane Katris</Link>
         </li>
         <li>
-          <Link href="https://levibutcher.com">https://levibutcher.com</Link>
+          <Link href="https://levibutcher.com/webring">Levi Butcher</Link>
         </li>
       </ul>
     </main>
